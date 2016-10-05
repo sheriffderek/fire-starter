37058a3ec12d9a4b9b5bd8e298e2ab256c23b482
sheriffderek <admin@sheriffderek.consulting>
2016-10-04 21:18:25 -0700

    Changes app name to Fire-starter
    - because I want this to be reuable starter
    - I’m sick of setting up structure
    - this can be my brunch skeleton or something

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'local-ideas',
+    modulePrefix: 'fire-starter',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
